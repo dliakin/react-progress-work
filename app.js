c6636e3f555a0cae8ebb04b03716684f07f32351
Dmitry Lyakin <d.lyakin@gmail.com>
2022-08-07 01:23:46 +0300

    Add progress bar implementation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector("#modal");
 const content = document.querySelector("#content");
 const backdrop = document.querySelector("#modal-backdrop");
+const progress = document.querySelector("#progress");
 
 const technologies = [
   {
@@ -13,7 +14,7 @@ const technologies = [
     title: "CSS",
     description: "CSS TEXT",
     type: "css",
-    done: true,
+    done: false,
   },
   {
     title: "JavaScript",
@@ -40,7 +41,7 @@ function closeCard() {
   modal.classList.remove("open");
 }
 
-function init() {
+function renderCards() {
   if (technologies.length === 0) {
     content.innerHTML = `<p class="empty">Технологий пока нет. Добавьте первую.</p>`;
   } else {
@@ -50,6 +51,32 @@ function init() {
 
 function toCard(tech) {
   return `<div class="card ${tech.done ? "done" : ""}"><h3>${tech.title}</h3></div>`;
+}
+
+function renderProgress() {
+  const percent = computeProgressPersent();
+  let background;
+  if (percent <= 30) {
+    background = "#e75a5a";
+  } else if (percent <= 70) {
+    background = "#f99415";
+  } else {
+    background = "#73ba3c";
+  }
+  progress.style.background = background;
+  progress.style.width = percent + "%";
+  progress.textContent = percent ? percent + "%" : "";
+}
+
+function computeProgressPersent() {
+  if (technologies.length === 0) return 0;
+  let doneCount = technologies.filter((tech) => tech.done === true).length;
+  return Math.round((100 * doneCount) / technologies.length);
+}
+
+function init() {
+  renderCards();
+  renderProgress();
 }
 
 init();
